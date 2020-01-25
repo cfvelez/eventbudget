aca196fffbd5cd7032b062a70deddd0a5643b1a0
Carlos Velez <cfvelez9@gmail.com>
2020-01-25 22:20:54 +0100

    #CICE-CFVELEZ: Base proyecto final - Heroku - Configuración base de datos

--- readme.txt
+++ readme.txt
@@ -6,3 +6,6 @@ npm i passport-local
 npm i passport-jwt
 npm i express-passport-logout
 npm i passport-facebook
+
+Database: heroku_ks3kv27n
+{ user: "heroku_ks3kv27n", account: "heroku_ks3kv27n" }
--- services/mongoose/index.js
+++ services/mongoose/index.js
@@ -1,14 +1,14 @@
 //conexión mongo
 const mongoose = require("mongoose");
-const DB_PORT = process.env.DB_PORT;
+const MONGODB_URI = process.env.MONGODB_URI;
 
 //conexión con Mongo
 mongoose
-  .connect(`mongodb://localhost:${DB_PORT}/eventbudget`, {
+  .connect(MONGODB_URI, {
     useNewUrlParser: true,
     useUnifiedTopology: true
   })
-  .then(() => console.log(`Connected to mongo on port ${DB_PORT}`))
+  .then(() => console.log(`Connected to mongo`))
   .catch(err => {
     throw err;
   });
