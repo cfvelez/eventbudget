2637f204e13247e8d30714cb8f456758fc8ee075
Carlos Velez <cfvelez9@gmail.com>
2020-06-21 12:35:39 +0200

    BACK - Version Beta - Filter main cities in Europe

--- controllers/cities/get.js
+++ controllers/cities/get.js
@@ -5,7 +5,7 @@ const City = require("../../models/City");
 //https://app.ticketmaster.com/discovery/v2/events/Z698xZ2qZa7OS.json?apikey=mfBX49RdBdlwQxFuWkWTAvV9pgi8YBcU
 module.exports = async (req, res) => {
   try {
-    const cities = await City.find({ country: "Spain" }, null, {
+    const cities = await City.find({ geonameid: "EVB" }, null, {
       sort: { name: 1 },
     });
     res.status(200).json(JSONResponse("ok", "Cities ready!", cities));
